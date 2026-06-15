60bc70db10233bb6278867466989d67c75319896
GiangVT <giangvt.it@thammythucuc.vn>
2026-06-15 17:23:53 +0700

    [UPDATE] update logo again

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -79,7 +79,7 @@ export default function Header() {
           </div>
           <div className="flex flex-col leading-none">
             <span className="gold-shimmer text-xl font-bold tracking-wide">
-              CoC VNO
+              VietNamOnline
             </span>
             <span className="text-xs text-stone-400 tracking-widest uppercase">
               Clash of Clans
